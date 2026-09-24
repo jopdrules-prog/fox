373ced0a44563d39cef8666349119c48f038682f
jopdrules-prog <jopdrules@gmail.com>
2026-09-25 02:57:00 +0900

    Build FOX Producer mobile PWA: sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="fox-producer-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","app.css","app.js","manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
